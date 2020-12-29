9347a3b5d06ed47a5bc0e054786f1b31e4d0955d
Dmitry <59758691+r8nes@users.noreply.github.com>
2020-12-29 12:51:25 +0300

    Update project without logic

--- js/main.js
+++ js/main.js
@@ -1,65 +1,81 @@
-const navMenu = document.getElementById('nav-menu'),
-toggleMenu = document.getElementById('nav-toggle'),
-closeMenu = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close");
 
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show');
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
 });
 
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show');
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
 });
 
-document.addEventListener('mousemove', move)
+document.addEventListener("mousemove", move);
 
 function move(e) {
-    this.querySelectorAll('.move').forEach(layer => {
-        const speed =layer.getAttribute('data-speed');
+  this.querySelectorAll(".move").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
 
-        const x = (window.innerWidth - e.pageX*speed)/120;
-        const y = (window.innerHeight - e.pageY*speed)/120;
+    const x = (window.innerWidth - e.pageX * speed) / 120;
+    const y = (window.innerHeight - e.pageY * speed) / 120;
 
-        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
 }
 
-gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 1, delay:1.2, y:10});
-gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 1.3, y:30, stagger: 0.2});
-gsap.from('.home__title', {opacity:0, duration:1,delay:1.5,y:30});
-gsap.from('.home__description', {opacity:0, duration:1,delay:1.7,y:30});
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.2,
+  y: 10,
+});
 
-new Swiper('.swiper-container', {
-    // Optional parameter
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 10,
-      depth: 200,
-      modifier: 1,
-      slideShadows: true,
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.3,
+  y: 30,
+  stagger: 0.2,
+});
+
+gsap.from(".home__title", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.5,
+  y: 30
+});
+gsap.from(".home__description", {
+  opacity: 0,
+  duration: 1,
+  delay: 1.7,
+  y: 30
+});
+
+new Swiper(".swiper-container", {
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 10,
     },
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-  
-    
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
     },
-  })
-
-
+  },
+});
